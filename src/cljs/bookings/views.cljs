12f8c5fac23616e42f93f59e8ec8f7b5b73cea41
Robin Nagpal <rnagpal@rockportnetworks.com>
2017-07-10 10:20:36 -0400

    Add some styling

--- src/cljs/bookings/views.cljs
+++ src/cljs/bookings/views.cljs
@@ -3,6 +3,7 @@
             [reagent.core :as r])
   )
 
+
 (defn size-selection []
   (let [matrix-size (r/atom 0)]
     [:div [:h2 "Select the size of matrix you want to play with"]
@@ -14,30 +15,29 @@
                :on-click #(do (re-frame/dispatch [:set-matrix-size @matrix-size])
                               (re-frame/dispatch [:set-game-step "game"]))} "Start"]]))
 
+
 (defn play-game []
   (let [matrix-size (re-frame/subscribe [:matrix-size])]
     [:div {:class "container"}
      [:h2 "Select the squares that don't have mines"]
-     [:div {:style {:background-color "red" :padding "30px 30px 30px 30px"}}
-      [:div {:style {:width "600px" :height "600px"}}
+     [:div {:class "col-lg-2 col-md-2" }]
+     [:div {:class "col-lg-8 col-md-8" :style {:padding "50px 0px 50px 0px"} }
+      [:div
        (do (println (str "Matrix Size : " @matrix-size))
            (->
              (for  [row (range @matrix-size)] (seq (range @matrix-size)))
              (println))
            (for [cell (range (* @matrix-size @matrix-size))]
              ^{:key cell} [:div {:style {:width (str (/ 100 @matrix-size) "%")
-                                         :height (str (/ 100 @matrix-size) "%")
-                                         :background-color "yellow"
-                                         :box-sizing "border-box"
-                                         :border "5px solid black"
-                                         :float "left"
-                                         :cursor "pointer"}}]))]]]))
+                                         :padding-top (str (/ 100 @matrix-size) "%")}
+                                 :class "mine-box"}]))]]
+     [:div {:class "col-lg-2 col-md-2" }]]))
 
 (defn home-panel []
   (let [name (re-frame/subscribe [:name])
         step (re-frame/subscribe [:game-step])]
     (fn []
-      [:div {:class "container" :style {:margin "100px 0px 0px 0px" :text-align "center"}}
+      [:div {:style {:margin "50px 0px 0px 0px" :text-align "center"}}
        (condp = @step
          "size-selection" [size-selection]
          "game" [play-game])])))
